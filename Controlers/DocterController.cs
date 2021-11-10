9e4df9cc5931ead83396d36fe555a10cece07719
navyanidamanuri <89773947+navyanidamanuri@users.noreply.github.com>
2021-11-10 18:09:26 +0100

    Add project files.

---
+++ Controlers/DocterController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstWebApplication.Controlers
+{
+    public class DocterController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult CheckFever()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CheckFever(string txtTemp)
+        {
+
+            int temp = int.Parse(txtTemp);
+            if (temp >= 100)
+                ViewBag.msg = "Person having Fever";
+            if (temp < 100)
+                ViewBag.msg = "Person having  no Fever";
+            return View();
+        }
+
+    }
+}
---
+++ Controlers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstWebApplication.Controlers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
