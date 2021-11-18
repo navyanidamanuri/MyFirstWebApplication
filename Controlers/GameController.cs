883b04222dce6f4c9c442a8dc7c40ff78b58c4f7
navyanidamanuri <89773947+navyanidamanuri@users.noreply.github.com>
2021-11-18 18:00:56 +0100

    changes in gessing

--- Controlers/GameController.cs
+++ Controlers/GameController.cs
@@ -23,26 +23,35 @@ namespace MyFirstWebApplication.Controlers
             int x = int.Parse(txtGame);
             if (x >= 1 && x <= 100)
             {
-                int n = Convert.ToInt32(HttpContext.Session.GetInt32("rndNumber"));
-                if(n==0)
+               
+                if (HttpContext.Session.IsAvailable)
                 {
-                    ViewBag.info = "Session expired.please refresh the page";
-                }
-                if (x == n)
-                {
-                    ViewBag.info = "Congrats, you won the game";
-                    Random rnd = new Random();
-                    HttpContext.Session.SetInt32("rndNumber", rnd.Next(1, 100));
-                }
-                else if (x > n)
-                    ViewBag.info = "Sorry, Given value greater to ganarated value";
-                else// (x < n)
-                    ViewBag.info = "Sorry, Given value lesser to ganarated value";
-                
+                    int n = Convert.ToInt32(HttpContext.Session.GetInt32("rndNumber"));
 
+                    if (n == 0)
+                    {
+                        ViewBag.info = "Session expired.please refresh the page";
+                    }
+                    if (x == n)
+                    {
+                        ViewBag.info = "Congrats, you won the game";
+                        Random rnd = new Random();
+                        HttpContext.Session.SetInt32("rndNumber", rnd.Next(1, 100));
+                    }
+                    else if (x > n)
+                        ViewBag.info = "Sorry, Given value greater to ganarated value";
+                    else// (x < n)
+                        ViewBag.info = "Sorry, Given value lesser to ganarated value";
+
+
+                }
+                else
+                    ViewBag.info = "Value must be in 1-100";
             }
             else
-                ViewBag.info = "Value must be in 1-100";
+            {
+                ViewBag.info = "Session expaired";
+            }
             return View();
         }
     }
